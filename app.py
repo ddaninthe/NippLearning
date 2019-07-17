b73f5bd178ac6a2a77c5f6bf6bdcc5b4087f420f
ddaninthe <d.daninthelopez@gmail.com>
2019-07-17 22:21:22 +0200

    Added Multi-Files Client-Server handling

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from tensorflow.python.keras import models
 import numpy as np
 from math import isclose
 import io
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -16,25 +17,30 @@ def root():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'image' not in request.form:
+    if 'images' not in request.form:
         abort(400)
 
-    image_encoded = request.form['image'].split(';')[1].split(',')[1]
-    image = Image.open(io.BytesIO(b64decode(image_encoded)))
+    results = []
+    request_images = json.loads(request.form['images'])
+    for form_image in request_images:
+        img_name = form_image['name']
 
-    # Remove alpha
-    image = image.convert('RGB')
+        image_encoded = form_image['src'].split(';')[1].split(',')[1]
+        image = Image.open(io.BytesIO(b64decode(image_encoded)))
 
-    image = np.reshape(image, (1, 3072))
+        # Remove alpha
+        image = image.convert('RGB')
 
-    model_name = "sgd_d64.32.1_lr0.2_e120_acc0.84_vacc0.65.h5"
-    model = models.load_model("./models/" + model_name)
+        image = np.reshape(image, (1, 3072))
 
-    value = model.predict(image)
+        model_name = "sgd_d64.32.1_lr0.2_e120_acc0.84_vacc0.65.h5"
+        model = models.load_model("./models/" + model_name)
 
-    if isclose(value, 0.0, rel_tol=1e-6):
-        prediction = "Un Homme"
-    else :
-        prediction = "Une Femme"
+        value = model.predict(image)
 
-    return prediction
+        if isclose(value, 0.0, rel_tol=1e-6):
+            results.append([img_name, "Homme"])
+        else:
+            results.append([img_name, "Femme"])
+
+    return json.dumps(results)
--- ml.py
+++ ml.py
@@ -9,25 +9,42 @@ import numpy as np
 
 
 def get_tensor_dir(opt: str, epochs: int,
-                   lr: float, decay:float,
-                   l_inputs, test_mode=False):
+                   l_inputs, lr: float = 0,
+                   decay: float = 0, mom: float = 0,
+                   test_mode=False):
 
     dir_name = "./logs/" + opt + "_d"
     for li in l_inputs:
         dir_name += str(li) + "."
-    dir_name += "1_lr" + str(lr) + "_dc" + str(decay) + "_e" + str(epochs)
+
+    dir_name = dir_name[:-1]
+
+    if lr != 0:
+        dir_name += "_lr" + str(lr)
+
+    if decay != 0:
+        dir_name += "_dc" + str(decay)
+    dir_name += "_e" + str(epochs)
+
+    if mom != 0:
+        dir_name += "_m" + str(mom)
 
     if test_mode:
         dir_name += "_test"
     return dir_name
 
 
-def get_file_path(optimizer: str, l_inputs, lr: float):
+def get_file_path(optimizer: str, l_inputs, lr: float = 0):
     file_path = "./models/" + optimizer + "_d"\
 
     for li in l_inputs:
         file_path += str(li) + "."
-    file_path += "1_lr" + str(lr) + "_e{epoch:03d}_acc{acc:.2f}_vacc{val_acc:.2f}.h5"
+
+    file_path += "1"
+    if lr != 0:
+        file_path += "_lr" + str(lr)
+
+    file_path += "_e{epoch:03d}_acc{acc:.2f}_vacc{val_acc:.2f}.h5"
     return file_path
 
 
@@ -45,7 +62,7 @@ if __name__ == "__main__":
     model = models.Sequential()
 
     # Layers
-    layer_inputs = [64, 32]
+    layer_inputs = [32]
     for i in range(len(layer_inputs)):
         if i == 0:
             model.add(layers.Dense(layer_inputs[i],
@@ -58,26 +75,26 @@ if __name__ == "__main__":
                            activation=activations.sigmoid))
 
     # Compile
-    learning_rate = .2
-    decay = .002
-    opt = "sgd"
-    optimizer = optimizers.SGD(lr=learning_rate, decay=decay)
+    opt = "adam"
+    optimizer = optimizers.Adam()
 
     model.compile(loss=losses.binary_crossentropy,
                   optimizer=optimizer,
                   metrics=['accuracy'])
 
     # Callbacks
-    epochs = 150
-    tensor_cb = callbacks.TensorBoard(log_dir=get_tensor_dir(opt=opt,
-                                                             epochs=epochs,
-                                                             lr=learning_rate,
-                                                             decay=decay,
-                                                             l_inputs=layer_inputs))
+    epochs = 300
+    log_dir = get_tensor_dir(opt=opt,
+                             epochs=epochs,
+                             test_mode=False,
+                             l_inputs=layer_inputs)
+    tensor_cb = callbacks.TensorBoard(log_dir=log_dir)
 
-    early_cb = callbacks.EarlyStopping(monitor='acc', patience=3, min_delta=0.1)
+    # Stops the training if no improvement
+    early_cb = callbacks.EarlyStopping(monitor='acc', patience=3, min_delta=0.05)
 
-    file_path = get_file_path(optimizer=opt, l_inputs=layer_inputs, lr=learning_rate)
+    # Saves model while training if [monitor] param is better than the [period] previous epochs
+    file_path = get_file_path(optimizer=opt, l_inputs=layer_inputs)
     checkpoint_cp = callbacks.ModelCheckpoint(filepath=file_path,
                                               monitor="acc",
                                               save_best_only=True,
@@ -85,7 +102,6 @@ if __name__ == "__main__":
 
     model.fit(x_data, y_data,
               epochs=epochs,
-              batch_size=128,
+              batch_size=64,
               validation_split=0.2,
               callbacks=[tensor_cb])
-
